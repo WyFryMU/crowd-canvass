45a99c6016cf7e775402f6147e50e693e765351b
BuildTools <unconfigured@null.spigotmc.org>
2021-04-28 14:29:30 -0500

    map infowindows work, just need to add actual content

--- public/firebasemap.js
+++ public/firebasemap.js
@@ -3,8 +3,28 @@
 // failed.", it means you probably did not give permission for the browser to
 // locate you.
 let map, infoWindow;
-let markersArray = [];
-
+var markersArray = [];
+const contentString =
+'<div id="content">' +
+'<div id="siteNotice">' +
+"</div>" +
+'<h1 id="firstHeading" class="firstHeading">Uluru</h1>' +
+'<div id="bodyContent">' +
+"<p><b>Uluru</b>, also referred to as <b>Ayers Rock</b>, is a large " +
+"sandstone rock formation in the southern part of the " +
+"Northern Territory, central Australia. It lies 335&#160;km (208&#160;mi) " +
+"south west of the nearest large town, Alice Springs; 450&#160;km " +
+"(280&#160;mi) by road. Kata Tjuta and Uluru are the two major " +
+"features of the Uluru - Kata Tjuta National Park. Uluru is " +
+"sacred to the Pitjantjatjara and Yankunytjatjara, the " +
+"Aboriginal people of the area. It has many springs, waterholes, " +
+"rock caves and ancient paintings. Uluru is listed as a World " +
+"Heritage Site.</p>" +
+'<p>Attribution: Uluru, <a href="https://en.wikipedia.org/w/index.php?title=Uluru&oldid=297882194">' +
+"https://en.wikipedia.org/w/index.php?title=Uluru</a> " +
+"(last visited June 22, 2009).</p>" +
+"</div>" +
+"</div>";
 
 const firebaseConfig = {
   apiKey: "AIzaSyAgEK90feSQbkemjfXMCWwTh-Mid7lAq1Y",
@@ -16,9 +36,9 @@ const firebaseConfig = {
   appId: "1:805578793962:web:a4784e3e4647f013d50893",
   };
 
-  firebase.initializeApp(firebaseConfig);
+firebase.initializeApp(firebaseConfig);
 
-  function initMap() {
+function initMap() {
   //starting map
   map = new google.maps.Map(document.getElementById("map"), {
     center: { lat: 43.0389, lng: -87.906 },
@@ -53,97 +73,103 @@ const firebaseConfig = {
   //});
 
 
-function handleLocationError(browserHasGeolocation, infoWindow, pos) {
-  infoWindow.setPosition(pos);
-  infoWindow.setContent(
-    browserHasGeolocation
-      ? "Error: The Geolocation service failed."
-      : "Error: Your browser doesn't support geolocation."
-  );
-  infoWindow.open(map);
-}
-// Marker with text on click
-const chicago = { lat: 41.881, lng: -87.623 };
-const contentString =
-    '<div id="content">' +
-    '<div id="siteNotice">' +
-    "</div>" +
-    '<h1 id="firstHeading" class="firstHeading">Uluru</h1>' +
-    '<div id="bodyContent">' +
-    "<p><b>Uluru</b>, also referred to as <b>Ayers Rock</b>, is a large " +
-    "sandstone rock formation in the southern part of the " +
-    "Northern Territory, central Australia. It lies 335&#160;km (208&#160;mi) " +
-    "south west of the nearest large town, Alice Springs; 450&#160;km " +
-    "(280&#160;mi) by road. Kata Tjuta and Uluru are the two major " +
-    "features of the Uluru - Kata Tjuta National Park. Uluru is " +
-    "sacred to the Pitjantjatjara and Yankunytjatjara, the " +
-    "Aboriginal people of the area. It has many springs, waterholes, " +
-    "rock caves and ancient paintings. Uluru is listed as a World " +
-    "Heritage Site.</p>" +
-    '<p>Attribution: Uluru, <a href="https://en.wikipedia.org/w/index.php?title=Uluru&oldid=297882194">' +
-    "https://en.wikipedia.org/w/index.php?title=Uluru</a> " +
-    "(last visited June 22, 2009).</p>" +
-    "</div>" +
-    "</div>";
-  const infowindow = new google.maps.InfoWindow({
-    content: contentString,
-  });
-  const marker = new google.maps.Marker({
-    position: chicago,
-    map,
-    title: "Chicago",
-  });
-  marker.addListener("click", () => {
-    infowindow.open(map, marker);
-  });
-  //Geocode 
-  const geocoder = new google.maps.Geocoder();
-  //adding events to map
-  populateMap(geocoder, map);
-  markersArray[i].addListener("click", () => {
-    infowindow.open(map, marker);
-  });
+  function handleLocationError(browserHasGeolocation, infoWindow, pos) {
+    infoWindow.setPosition(pos);
+    infoWindow.setContent(
+      browserHasGeolocation
+        ? "Error: The Geolocation service failed."
+        : "Error: Your browser doesn't support geolocation."
+    );
+    infoWindow.open(map);
+  }
+  // Marker with text on click
+  const chicago = { lat: 41.881, lng: -87.623 };
+
+    const infowindow = new google.maps.InfoWindow({
+      content: contentString,
+    });
+    const marker = new google.maps.Marker({
+      position: chicago,
+      map,
+      title: "Chicago",
+    });
+    marker.addListener("click", () => {
+      infowindow.open(map, marker);
+    });
+    //console.log("marker");
+    //console.log(marker);
+    //Geocode 
+    const geocoder = new google.maps.Geocoder();
+    //adding events to map
+    populateMap(geocoder, map);
+    //console.log(markersArray);
+    setTimeout(
+      function(){
+        for(var i = 0; i < markersArray.length; i++){
+          google.maps.event.addListener(markersArray[i], 'click', function(){
+            this.infoWindow.open(map, this);
+          });
+          
+        }
+      }, 3000); //wait 3 seconds before running this code so the array can populate
 }
 
 function populateMap(geocoder, resultsMap) {  
-//Service Events map population
-const db = firebase.firestore();
-db.collection("serviceEvents").get().then(function(querySnapshot) {
-  querySnapshot.forEach(function(doc) {
-      console.log(doc.get("route1Addresses"));
-      var address = doc.get("route1Addresses");
-      geocoder.geocode({ address: address }, (results, status) => {
-      if (status === "OK") {
-      var marker = new google.maps.Marker({
-        map: resultsMap,
-        position: results[0].geometry.location,
-      });
-      markersArray.push(marker);
-    }/* else {
-      alert("Geocode was not successful for the following reason: " + status);
-    }*/
+  var lat, long;
+  //Service Events map population
+  const db = firebase.firestore();
+  db.collection("serviceEvents").get().then(function(querySnapshot) {
+    querySnapshot.forEach(function(doc) {
+        //console.log(doc.get("route1Addresses"));
+        var address = doc.get("route1Addresses");
+        geocoder.geocode({ address: address }, (results, status) => {
+        if (status === "OK") {
+          //console.log(results[0].geometry.location);
+          lat = results[0].geometry.location.lat();
+          long = results[0].geometry.location.lng();
+          var marker = new google.maps.Marker({
+            position: results[0].geometry.location,
+            map: resultsMap,
+            latitude: lat,
+            longitude: long
+        });
+        marker.infoWindow = new google.maps.InfoWindow({
+          content: contentString
+        });
+        //console.log(results[0].geometry.location);
+        markersArray.push(marker);
+      }/* else {
+        alert("Geocode was not successful for the following reason: " + status);
+      }*/
+    });
+    });
   });
+  //Canvass Events map population
+  db.collection("canvassEvents").get().then(function(querySnapshot) {
+    querySnapshot.forEach(function(doc) {
+        //console.log(doc.get("route1Addresses"));
+        var address = doc.get("route1Addresses");
+        geocoder.geocode({ address: address }, (results, status) => {
+          if (status === "OK") {
+            lat = results[0].geometry.location.lat();
+            long = results[0].geometry.location.lng();
+            var marker = new google.maps.Marker({
+              position: results[0].geometry.location,
+              map: resultsMap,
+              latitude: lat,
+              longitude: long
+            });
+            marker.infoWindow = new google.maps.InfoWindow({
+              content: contentString
+            });
+            markersArray.push(marker);
+          }/* else {
+        alert("Geocode was not successful for the following reason: " + status);
+      }*/
+        });
+    });
   });
-});
-//Canvass Events map population
-db.collection("canvassEvents").get().then(function(querySnapshot) {
-  querySnapshot.forEach(function(doc) {
-      console.log(doc.get("route1Addresses"));
-      var address = doc.get("route1Addresses");
-      geocoder.geocode({ address: address }, (results, status) => {
-      if (status === "OK") {
-      var marker = new google.maps.Marker({
-        map: resultsMap,
-        position: results[0].geometry.location,
-      });
-      markersArray.push(marker);
-    }/* else {
-      alert("Geocode was not successful for the following reason: " + status);
-    }*/
-  });
-  });
-});
-  }
+}
 
 
 
