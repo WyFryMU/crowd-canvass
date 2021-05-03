21903fcaebe21ab5de6331268cb7d5f094dd1e8f
BuildTools <unconfigured@null.spigotmc.org>
2021-05-03 02:05:13 -0500

    added change password in account manage

--- public/accountManage.js
+++ public/accountManage.js
@@ -1,3 +1,4 @@
+var logoutFlag = false;
 (function() {
 	console.log("loaded accountManage script");
     const firebaseConfig = {
@@ -12,9 +13,40 @@
 		
 	firebase.initializeApp(firebaseConfig);
     const logout = document.getElementById("logout");
+	const newPassword = document.getElementById("newPassword");
+	const confirmNewPassword = document.getElementById("confirmNewPassword");
+	const changePassword = document.getElementById("changePassword");
+	console.log(newPassword);
+	console.log(newPassword.value);
 	
+	changePassword.addEventListener('click', e => {
+        if(newPassword.value == confirmNewPassword.value){
+			console.log(newPassword);
+			console.log(confirmNewPassword);
+			var user = firebase.auth().currentUser;
+			
+
+			user.updatePassword(newPassword.value).then(function() {
+			// Update successful.
+				alert("Password has been sucessfully changed. Please relog.");
+				firebase.auth().signOut();
+			}).catch(function(error) {
+			// An error happened.
+				alert(error);
+			});
+		}else{
+			console.log(newPassword);
+			console.log(confirmNewPassword);
+			//console.log(newPassword);
+			console.log(confirmNewPassword.value);
+			//alert("New passwords do not match.")
+		}
+        
+    });
+
     //logout event
     logout.addEventListener('click', e => {
+		logoutFlag = true;
         firebase.auth().signOut();
         window.location = "index.html";
     });
@@ -25,9 +57,11 @@
 			console.log(firebaseUser.uid);
 			//window.location.href = "accountMainPageAfterSignIn.html";
 		}else{
-			console.log("not logged in");
-            //alert("Please sign in.");
-            //window.location = "signUpsignIn.html";
+			if(logoutFlag){
+				window.location = "index.html";
+			}else{
+				window.location = "signUpsignIn.html";	
+			}
 		}
 	});
 }());
